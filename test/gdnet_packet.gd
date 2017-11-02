58dac5aa63a711e3cff9586667c00a44b924cb8b
James McLean <empyreanx@zoho.com>
2017-11-02 12:17:21 -0400

    Another test assertion

--- test/gdnet_packet.gd
+++ test/gdnet_packet.gd
@@ -5,6 +5,7 @@ func tests():
     var packet = GDNetPacket.new()
     packet.push_int16(24)
     packet.push_uint32(43)
+    packet.push_uint64(64)
 
     var value = packet.pop_int16()
     print("Value ", value)
@@ -15,6 +16,9 @@ func tests():
     print("Value ", value)
 
     assert_eq(value, 43, "pop 43")
+
+    var value = packet.pop_uint64()
+    print("Value ", value)
 
     #assert_true(true, "Passing assertion")
     #assert_true(false, "Failing assertion")
