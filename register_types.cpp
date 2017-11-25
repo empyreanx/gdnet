6c1680d77ecbd595cb16ac1a91213cce12ecc9ed
PerduGames <perdugames@gmail.com>
2017-11-25 17:47:42 -0200

    Update register_types.cpp

--- register_types.cpp
+++ register_types.cpp
@@ -1,10 +1,10 @@
 /* register_types.cpp */
 
 #include "error_macros.h"
-#include "object_type_db.h"
+#include "class_db.h"
 #include "register_types.h"
 
-#include "enet/enet.h"
+#include "penet/penet.h"
 
 #include "gdnet_address.h"
 #include "gdnet_event.h"
@@ -12,16 +12,16 @@
 #include "gdnet_peer.h"
 
 void register_gdnet_types() {
-	ObjectTypeDB::register_virtual_type<GDNetPeer>();
-	ObjectTypeDB::register_virtual_type<GDNetEvent>();
-	ObjectTypeDB::register_virtual_type<GDNetMessage>();
-	ObjectTypeDB::register_type<GDNetHost>();
-	ObjectTypeDB::register_type<GDNetAddress>();
-	
-	if (enet_initialize() != 0)
-		ERR_EXPLAIN("Unable to initialize ENet");
+	ClassDB::register_virtual_class<GDNetPeer>();
+	ClassDB::register_virtual_class<GDNetEvent>();
+	ClassDB::register_virtual_class<GDNetMessage>();
+	ClassDB::register_class<GDNetHost>();
+	ClassDB::register_class<GDNetAddress>();
+
+	if (penet_initialize() != 0)
+		ERR_EXPLAIN("Unable to initialize PENet");
 }
 
 void unregister_gdnet_types() {
-	enet_deinitialize();
+	penet_deinitialize();
 }
