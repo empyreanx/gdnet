8a16b99c5f2c7e5bcb6242f021fa6c4425bb59e9
PerduGames <perdugames@gmail.com>
2017-11-25 17:41:36 -0200

    Update gdnet_address.h

--- gdnet_address.h
+++ gdnet_address.h
@@ -7,26 +7,26 @@
 #include "ustring.h"
 
 class GDNetAddress : public Reference {
-	
-	OBJ_TYPE(GDNetAddress,Reference);
-	
+
+	GDCLASS(GDNetAddress,Reference);
+
 	String _host;
 	int _port;
-	
+
 protected:
 
 	static void _bind_methods();
-	
+
 public:
-	
+
 	GDNetAddress() : _port(0) { }
-	
+
 	String get_host() const { return _host; }
 	void set_host(const String& host) { _host = host; }
-	
+
 	int get_port() const { return _port; }
 	void set_port(int port) { _port = port; }
-	
+
 };
 
 #endif
