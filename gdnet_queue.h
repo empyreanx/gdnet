6d669d1b3c676cafab4ae9a27bcb441c162162ec
PerduGames <perdugames@gmail.com>
2017-11-25 17:47:18 -0200

    Update gdnet_queue.h

--- gdnet_queue.h
+++ gdnet_queue.h
@@ -8,10 +8,10 @@
 template<class T, int SIZE = 1024>
 class GDNetQueue {
 	T* items[SIZE];
-	
+
 	int read_pos;
 	int write_pos;
-	
+
 public:
 
 	bool is_empty() {
@@ -30,17 +30,17 @@ public:
 		else
 			return 0;
 	}
-	
+
 	void push(T* item) {
 		ERR_FAIL_COND(is_full());
-				
+
 		items[write_pos] = item;
 		write_pos = (write_pos + 1) % SIZE;
 	}
 
 	T* pop() {
 		ERR_FAIL_COND_V(is_empty(), NULL);
-		
+
 		T* item = items[read_pos];
 		read_pos = (read_pos + 1) % SIZE;
 		return item;
