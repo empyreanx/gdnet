68c5e02dd5d5b16a0508e3101fc6d1918c9851bb
PerduGames <perdugames@gmail.com>
2017-11-25 17:44:51 -0200

    Update gdnet_message.cpp

--- gdnet_message.cpp
+++ gdnet_message.cpp
@@ -2,15 +2,15 @@
 
 #include "gdnet_message.h"
 
-GDNetMessage::GDNetMessage(Type type) : 
+GDNetMessage::GDNetMessage(Type type) :
 	_type(type),
-	_broadcast(false), 
+	_broadcast(false),
 	_peer_id(0),
 	_channel_id(0) {
 }
 
 void GDNetMessage::_bind_methods() {
-	BIND_CONSTANT(UNSEQUENCED);
-	BIND_CONSTANT(SEQUENCED);
-	BIND_CONSTANT(RELIABLE);
+	BIND_ENUM_CONSTANT(UNSEQUENCED);
+	BIND_ENUM_CONSTANT(SEQUENCED);
+	BIND_ENUM_CONSTANT(RELIABLE);
 }
