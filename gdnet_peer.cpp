66045da138d3b362048d6b58b612342fdb858eb7
PerduGames <perdugames@gmail.com>
2017-11-25 17:46:34 -0200

    Update gdnet_peer.cpp

--- gdnet_peer.cpp
+++ gdnet_peer.cpp
@@ -1,53 +1,142 @@
-/* gdnet_peer.h */
+/* gdnet_peer.cpp */
 
-#ifndef GDNET_PEER_H
-#define GDNET_PEER_H
+#include "gdnet_peer.h"
 
-#include "error_macros.h"
-#include "reference.h"
-#include "variant.h"
+GDNetPeer::GDNetPeer(GDNetHost* host, PENetPeer* peer) : _host(host), _peer(peer) {
+	_host->reference();
+}
 
-#include "penet/penet.h"
+GDNetPeer::~GDNetPeer() {
+	_host->unreference();
+}
 
-#include "gdnet_address.h"
-#include "gdnet_host.h"
-#include "gdnet_message.h"
+int GDNetPeer::get_peer_id() {
+	ERR_FAIL_COND_V(_host->_host == NULL, -1);
+	return (int)(_peer - _host->_host->peers);
+}
 
-class GDNetHost;
+Ref<GDNetAddress> GDNetPeer::get_address() {
+	Ref<GDNetAddress> address = memnew(GDNetAddress);
+	address->set_port(_peer->address.port);
 
-class GDNetPeer : public Reference {
+	char ip[64];
+	penet_address_get_host_ip(&_peer->address, ip, 64);
+	address->set_host(ip);
 
-	GDCLASS(GDNetPeer,Reference);
+	return address;
+}
 
-	GDNetHost* _host;
-	PENetPeer* _peer;
+int GDNetPeer::get_avg_rtt() {
+	ERR_FAIL_COND_V(_host->_host == NULL, -1);
+	return _peer->roundTripTime;
+}
 
-protected:
+void GDNetPeer::ping() {
+	ERR_FAIL_COND(_host->_host == NULL);
 
-	static void _bind_methods();
+	_host->acquireMutex();
 
-public:
+	penet_peer_ping(_peer);
 
-	GDNetPeer(GDNetHost* host, PENetPeer* peer);
-	~GDNetPeer();
+	_host->releaseMutex();
+}
 
-	int get_peer_id();
+void GDNetPeer::reset() {
+	ERR_FAIL_COND(_host->_host == NULL);
 
-	Ref<GDNetAddress> get_address();
+	_host->acquireMutex();
 
-	int get_avg_rtt();
+	penet_peer_reset(_peer);
 
-	void ping();
-	void reset();
+	_host->releaseMutex();
+}
 
-	void gdnet_disconnect(int data = 0);
-	void disconnect_later(int data = 0);
-	void disconnect_now(int data = 0);
+void GDNetPeer::gdnet_disconnect(int data) {
+	ERR_FAIL_COND(_host->_host == NULL);
 
-	void send_packet(const PoolByteArray& packet, int channel_id = 0, int type = GDNetMessage::UNSEQUENCED);
-	void send_var(const Variant& var, int channel_id = 0, int type = GDNetMessage::UNSEQUENCED);
+	_host->acquireMutex();
 
-	void set_timeout(int limit, int min_timeout, int max_timeout);
-};
+	penet_peer_disconnect(_peer, data);
 
-#endif
+	_host->releaseMutex();
+}
+
+void GDNetPeer::disconnect_later(int data) {
+	ERR_FAIL_COND(_host->_host == NULL);
+
+	_host->acquireMutex();
+
+	penet_peer_disconnect_later(_peer, data);
+
+	_host->releaseMutex();
+}
+
+void GDNetPeer::disconnect_now(int data) {
+	ERR_FAIL_COND(_host->_host == NULL);
+
+	_host->acquireMutex();
+
+	penet_peer_disconnect_now(_peer, data);
+
+	_host->releaseMutex();
+}
+
+void GDNetPeer::send_packet(const PoolByteArray& packet, int channel_id, int type) {
+	ERR_FAIL_COND(_host->_host == NULL);
+
+	GDNetMessage* message = memnew(GDNetMessage((GDNetMessage::Type)type));
+	message->set_peer_id(get_peer_id());
+	message->set_channel_id(channel_id);
+	message->set_packet(packet);
+	_host->_message_queue.push(message);
+}
+
+void GDNetPeer::send_var(const Variant& var, int channel_id, int type) {
+	ERR_FAIL_COND(_host->_host == NULL);
+
+	int len;
+
+	Error err = encode_variant(var, NULL, len);
+
+	ERR_FAIL_COND(err != OK || len == 0);
+
+	GDNetMessage* message = memnew(GDNetMessage((GDNetMessage::Type)type));
+	message->set_peer_id(get_peer_id());
+	message->set_channel_id(channel_id);
+
+	PoolByteArray packet;
+	packet.resize(len);
+
+	PoolByteArray::Write w = packet.write();
+	err = encode_variant(var, &w[0], len);
+
+	ERR_FAIL_COND(err != OK);
+
+	message->set_packet(packet);
+
+	_host->_message_queue.push(message);
+}
+
+void GDNetPeer::set_timeout(int limit, int min_timeout, int max_timeout) {
+	ERR_FAIL_COND(_host->_host == NULL);
+
+	_host->acquireMutex();
+
+	penet_peer_timeout(_peer, limit, min_timeout, max_timeout);
+
+	_host->releaseMutex();
+}
+
+void GDNetPeer::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_peer_id"), &GDNetPeer::get_peer_id);
+	ClassDB::bind_method(D_METHOD("get_address"), &GDNetPeer::get_address);
+	ClassDB::bind_method(D_METHOD("get_avg_rtt"), &GDNetPeer::get_avg_rtt);
+	ClassDB::bind_method(D_METHOD("ping"), &GDNetPeer::ping);
+	ClassDB::bind_method(D_METHOD("reset"), &GDNetPeer::reset);
+	ClassDB::bind_method(D_METHOD("gdnet_disconnect"), &GDNetPeer::gdnet_disconnect,DEFVAL(0));
+	ClassDB::bind_method(D_METHOD("disconnect_later"), &GDNetPeer::disconnect_later,DEFVAL(0));
+	ClassDB::bind_method(D_METHOD("disconnect_now"), &GDNetPeer::disconnect_now,DEFVAL(0));
+	ClassDB::bind_method(D_METHOD("send_packet"), &GDNetPeer::send_packet,DEFVAL(0),DEFVAL(GDNetMessage::UNSEQUENCED));
+	ClassDB::bind_method(D_METHOD("send_var"), &GDNetPeer::send_var,DEFVAL(0),DEFVAL(GDNetMessage::UNSEQUENCED));
+	ClassDB::bind_method(D_METHOD("set_timeout"), &GDNetPeer::set_timeout);
+}
